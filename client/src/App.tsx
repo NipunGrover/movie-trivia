6e1dfc59982f6c97300fe9a89a9d18df909ea4de
Nipun Grover <nipungrover2003@outlook.com>
2025-06-16 16:05:54 -0400

    - removed app test file - removed default vite app.tsx content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,33 +14,7 @@ function App() {
 
   return (
     <main>
-      <div className="flex justify-center">
-        <a href="https://vite.dev" rel="noreferrer" target="_blank">
-          <img alt="Vite logo" className="logo" src={VITE_LOGO} />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img
-            alt="React logo"
-            className="logo react logo-spin"
-            src={REACT_LOGO}
-          />
-        </a>
-      </div>
-      <div className="flex flex-col items-center justify-center text-center">
-        <h1 className="text-3xl font-bold">Vite + React</h1>
-        <button
-          className="my-2 rounded-sm bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700"
-          onClick={() => {
-            const incrementValue = 1;
-            setCount(prevCount => prevCount + incrementValue);
-          }}
-          type="button">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <div className="flex justify-center">hello world</div>
     </main>
   );
 }
