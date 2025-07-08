b69ebd188c3f817b4745fcf3d2ce3a352fca02a2
Nipun Grover <nipungrover2003@outlook.com>
2025-07-08 15:02:21 -0400

    set up flask backend

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
+import os
+from socket import SocketIO
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'dev-secret-key')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def hello():
